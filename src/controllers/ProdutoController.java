8aef769a3f1a429b7be85fa5edeb7f1653e8a5ea
Henrique20o <henriquepc7@gmail.com>
2025-05-20 16:24:19 -0300

    Update ProdutoController.java

--- src/controllers/ProdutoController.java
+++ src/controllers/ProdutoController.java
@@ -1,5 +1,18 @@
 package controllers;
 
+import models.DAO.ProdutosDAO;
+import models.entity.ProdutosEntity;
+
 public class ProdutoController {
+    private ProdutosDAO dao;
+
+    public ProdutoController(ProdutosDAO dao){
+        this.dao = dao;
+    }
+
+    public void cadastrarProduto(String nome, long categoria, long preco, long quantidade){
+        ProdutosEntity produto = new ProdutosEntity(nome,categoria, preco, quantidade);
+        dao.cadastro(produto);
+    }
 
 }
