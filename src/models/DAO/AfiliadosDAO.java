d245d3c1cf9e67b5ce5b12136902f72c0c842b31
Henrique20o <henriquepc7@gmail.com>
2025-05-28 10:42:15 -0300

    Update AfiliadosDAO.java

--- src/models/DAO/AfiliadosDAO.java
+++ src/models/DAO/AfiliadosDAO.java
@@ -1,9 +1,27 @@
 package models.DAO;
 
+import connection.MySQLConnection;
+import models.entity.AfiliadoEntity;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AfiliadosDAO {
 
-    public void cadastroAfiliado(){
-        String sql = ""
+    public void cadastroAfiliado(AfiliadoEntity afiliado){
+        String sql = "INSERT INTO afiliados (nome, sobrenome, cpf, nascimento) VALUES (?, ?, ?, ?)";
+
+        try(PreparedStatement stmt = MySQLConnection.getConnection().prepareStatement(sql)){
+            stmt.setString(1, afiliado.getNome());
+            stmt.setString(2, afiliado.getSobrenome());
+            stmt.setString(3, afiliado.getCpf());
+            stmt.setString(4, afiliado.getNascimento());
+
+            stmt.executeUpdate();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
 }
